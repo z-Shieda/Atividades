a24bcf97f95276f99bfd8f271862daecdb112b96
z-Shieda <zdanniel.k@gmail.com>
2024-09-27 15:39:35 -0300

    Lista

---
+++ Exercicio 11.py
@@ -0,0 +1,18 @@
+n1 = int(input("Digite um número inteiro"))
+n2 = int(input("Digite outro número inteiro"))
+nr = float(input("Digite um número real"))
+
+dobro = n1 * 2
+metade = n2 / 2
+triplo = n1 * 3
+cubo = nr **3
+
+a = dobro + metade
+a_resp = print("o produto do dobro do primeiro com metade do segundo. \nO Resultado é: {}".format(a))
+
+b = triplo + nr
+b_resp = print(("a soma do triplo do primeiro com o terceiro.\nO Resolutado é: {}".format(b)))
+
+c = cubo
+c_resp = print("o terceiro elevado ao cubo.\nO Resultado é: {}".format(c))
+
