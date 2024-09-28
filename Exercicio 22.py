9cfdeaf1b0bab73871f8da086d028f7b4e4bc7d9
z-Shieda <zdanniel.k@gmail.com>
2024-09-28 12:03:18 -0300

    Lista

---
+++ Exercicio 22.py
@@ -0,0 +1,7 @@
+numero1 = input("Escreva um número")
+numero2 = input("Mais um")
+numero3 = input("Agora o ultimo número")
+
+maior = max(numero1, numero2, numero3)
+
+print(f"O maior número dentre esses é o {maior}")
---
+++ Exercicio 23.py
@@ -0,0 +1,8 @@
+numero1 = input("Escreva um número")
+numero2 = input("Mais um")
+numero3 = input("Agora o ultimo número")
+
+maior = max(numero1, numero2, numero3)
+menor = min(numero1, numero2, numero3)
+
+print(f"O maior número dentre esses é o {maior} e o menor é o {menor}")
